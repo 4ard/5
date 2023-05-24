d1cbacfc73494d446660321b8cdddcdaf4cec682
4ard <112971420+4ard@users.noreply.github.com>
2023-05-24 10:22:58 +0300

    form.php

--- form.php
+++ form.php
@@ -1,6 +1,6 @@
 <style>
 body{
-    background-color: #fcee0a;
+    background-color: white;
     display: flex;
     justify-content:center;
     margin-top:5%;
@@ -9,7 +9,7 @@ body{
 .main{
     padding: 40px;
     width: 250px;
-    background-color: #00ffd2;
+    background-color: white;
     border: 2px solid #fd0130;
 }
 
